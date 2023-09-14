89bd65635d9f8cb778beaec18d95ba5a3cb02603
Ars Vladimirov <grokhael@gmail.com>
2023-09-14 22:46:48 +0600

    add preview

--- src/convert.py
+++ src/convert.py
@@ -1,80 +1,125 @@
-import supervisely as sly
+# https://www.kaggle.com/datasets/qramkrishna/corn-leaf-infection-dataset
+
+import csv
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
+from collections import defaultdict
 
-from tqdm import tqdm
+import cv2
+import numpy as np
+import supervisely as sly
+from dotenv import load_dotenv
+from supervisely.io.fs import (
+    file_exists,
+    get_file_ext,
+    get_file_name,
+    get_file_name_with_ext,
+    get_file_size,
+    mkdir,
+    remove_dir,
+)
 
-def download_dataset(teamfiles_dir: str) -> str:
-    """Use it for large datasets to convert them on the instance"""
-    api = sly.Api.from_env()
-    team_id = sly.env.team_id()
-    storage_dir = sly.app.get_data_dir()
 
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, str):
-        parsed_url = urlparse(s.DOWNLOAD_ORIGINAL_URL)
-        file_name_with_ext = os.path.basename(parsed_url.path)
-        file_name_with_ext = unquote(file_name_with_ext)
-
-        sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-        local_path = os.path.join(storage_dir, file_name_with_ext)
-        teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-
-        fsize = api.file.get_directory_size(team_id, teamfiles_dir)
-        with tqdm(desc=f"Downloading '{file_name_with_ext}' to buffer...", total=fsize) as pbar:
-            api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-        dataset_path = unpack_if_archive(local_path)
-
-    if isinstance(s.DOWNLOAD_ORIGINAL_URL, dict):
-        for file_name_with_ext, url in s.DOWNLOAD_ORIGINAL_URL.items():
-            local_path = os.path.join(storage_dir, file_name_with_ext)
-            teamfiles_path = os.path.join(teamfiles_dir, file_name_with_ext)
-
-            if not os.path.exists(get_file_name(local_path)):
-                fsize = api.file.get_directory_size(team_id, teamfiles_dir)
-                with tqdm(
-                    desc=f"Downloading '{file_name_with_ext}' to buffer...",
-                    total=fsize,
-                    unit="B",
-                    unit_scale=True,
-                ) as pbar:
-                    api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
-
-                sly.logger.info(f"Start unpacking archive '{file_name_with_ext}'...")
-                unpack_if_archive(local_path)
-            else:
-                sly.logger.info(
-                    f"Archive '{file_name_with_ext}' was already unpacked to '{os.path.join(storage_dir, get_file_name(file_name_with_ext))}'. Skipping..."
-                )
-
-        dataset_path = storage_dir
-    return dataset_path
-    
-def count_files(path, extension):
-    count = 0
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(extension):
-                count += 1
-    return count
-    
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    # project_name = "Corn Leaf Infection"
+    dataset_path = "/mnt/d/datasetninja-raw/corn-leaf-infection/Corn Disease detection"
+    anns_path = "/mnt/d/datasetninja-raw/corn-leaf-infection/Annotation-export.csv"
+    batch_size = 5
+    ds_name = "ds"
+    images_ext = ".jpg"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        tag_meta = subfolder_to_meta[subfolder]
+        tag = sly.Tag(tag_meta)
 
-    # return project
+        if subfolder == "Infected":
+            bboxes = im_name_to_bboxes[get_file_name_with_ext(image_path)]
 
+            for bbox in bboxes:
+                left = int(bbox[0])
+                top = int(bbox[1])
+                right = int(bbox[2])
+                bottom = int(bbox[3])
+                rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+                label = sly.Label(rect, obj_class)
+                labels.append(label)
 
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=[tag])
+
+    obj_class = sly.ObjClass("infected leaf", sly.Rectangle)
+
+    tag_healthy = sly.TagMeta("healthy", sly.TagValueType.NONE)
+    tag_infected = sly.TagMeta("infected", sly.TagValueType.NONE)
+
+    subfolder_to_meta = {"Healthy corn": tag_healthy, "Infected": tag_infected}
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[obj_class], tag_metas=[tag_healthy, tag_infected])
+    api.project.update_meta(project.id, meta.to_json())
+
+    im_name_to_bboxes = defaultdict(list)
+
+    with open(anns_path, "r") as file:
+        csvreader = csv.reader(file)
+        for idx, row in enumerate(csvreader):
+            if idx == 0:
+                continue
+
+            im_name_to_bboxes[row[0]].append(list(map(float, row[1:-1])))
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    for subfolder in os.listdir(dataset_path):
+        if subfolder == "temp":
+            continue
+        curr_images_path = os.path.join(dataset_path, subfolder)
+
+        images_names = [
+            im_name
+            for im_name in os.listdir(curr_images_path)
+            if get_file_ext(im_name) == images_ext
+        ]
+
+        progress = sly.Progress(
+            "Create dataset {}, add {} data".format(ds_name, subfolder), len(images_names)
+        )
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(curr_images_path, image_name) for image_name in images_names_batch
+            ]
+
+            # TODO =========================== must have, check EXIF Rotate 90 =========================
+            temp_img_pathes_batch = []
+            temp_folder = os.path.join(dataset_path, "temp")
+            mkdir(temp_folder)
+            for im_path in img_pathes_batch:
+                temp_img = cv2.imread(
+                    im_path,
+                    flags=cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH | cv2.IMREAD_IGNORE_ORIENTATION,
+                )
+                new_img_path = os.path.join(temp_folder, get_file_name_with_ext(im_path))
+                temp_img_pathes_batch.append(new_img_path)
+                cv2.imwrite(new_img_path, temp_img)
+
+            # TODO =======================================================================================
+
+            if subfolder == "Infected":
+                images_names_batch = ["infected" + "_" + im_name for im_name in images_names_batch]
+            img_infos = api.image.upload_paths(
+                dataset.id, images_names_batch, temp_img_pathes_batch
+            )
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in temp_img_pathes_batch]
+            remove_dir(temp_folder)
+            api.annotation.upload_anns(img_ids, anns)
+            progress.iters_done_report(len(images_names_batch))
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,39 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Corn Leaf Infection"
+PROJECT_NAME_FULL: str = "Corn Leaf Infection Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Agricultural(is_used=False)]
+CATEGORY: Category = Category.Agriculture()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection(), CVTask.Classification()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2020
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/qramkrishna/corn-leaf-infection-dataset"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 4003551
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/corn-leaf-infection-dataset"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[
+    Union[str, dict]
+] = "https://www.kaggle.com/datasets/qramkrishna/corn-leaf-infection-dataset/download?datasetVersionNumber=2"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -52,17 +54,23 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
 PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
-BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
+BLOGPOST: Optional[
+    Union[str, List[str], Dict[str, str]]
+] = "https://q-viper.github.io/2020/10/19/corn-infection-detection-data-preparation/"
+REPOSITORY: Optional[
+    Union[str, List[str], Dict[str, str]]
+] = "https://github.com/q-viper/Corn-Infection-Detection/tree/master"
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = "https://www.kaggle.com/qramkrishna/corn-leaf-infection-dataset"
+AUTHORS: Optional[List[str]] = ["Ramkrishna Acharya"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "classification sets": ["healthy", "infected"]
+}
 TAGS: Optional[List[str]] = None
 
 
